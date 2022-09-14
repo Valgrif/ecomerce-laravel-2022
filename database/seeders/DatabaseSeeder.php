6dad5736b90a910218a14c73543fe039f51780f2
Valgrif <dgh1288@gmail.com>
2022-09-14 13:56:43 +0100

    RoleFactory & corrections

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Product::factory(30)->create();
+
+        $admin_role = \App\Models\Role::factory()->create([
+            'name' => 'Admin'
+        ]);
+
+        $client_role = \App\Models\Role::factory()->create([
+            'name' => 'Client'
+        ]);
+
+        $manager_role = \App\Models\Role::factory()->create([
+            'name' => 'Manager'
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\Product::factory(30)->create();
     }
 }
