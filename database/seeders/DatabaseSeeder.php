010474806159236751ec88b24a670fc54dd41f44
Valgrif <dgh1288@gmail.com>
2022-09-15 10:05:47 +0100

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,24 +14,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+
         \App\Models\Product::factory(30)->create();
 
         $admin_role = \App\Models\Role::factory()->create([
             'name' => 'Admin'
         ]);
 
+        $admin_users = \App\Models\User::factory(2)->create();
+        foreach ($admin_users as $user){
+            $user->roles()->attach($admin_role);
+        }
+
         $client_role = \App\Models\Role::factory()->create([
             'name' => 'Client'
         ]);
+
+        $client_users = \App\Models\User::factory(200)->create();
+        foreach ($client_users as $user){
+            $user->roles()->attach($client_role);
+        }
 
         $manager_role = \App\Models\Role::factory()->create([
             'name' => 'Manager'
         ]);
 
+        $manager_users = \App\Models\User::factory(12)->create();
+        foreach ($manager_users as $user){
+            $user->roles()->attach($manager_role);
+        }
+
         $vip_role = \App\Models\Role::factory()->create([
             'name' => 'Vip'
         ]);
+
+        $vip_users = \App\Models\User::factory(40)->create();
+        foreach ($vip_users as $user){
+            $user->roles()->attach($vip_role);
+            $user->roles()->attach($client_role);
+        }
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
