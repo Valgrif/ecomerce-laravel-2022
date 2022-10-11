9a48c32a77620dc98a80e3cf77d575876b35c80e
Valgrif <dgh1288@gmail.com>
2022-10-11 12:57:28 +0100

    Tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\Category;
+use App\Models\Product;
 
 class ExampleTest extends TestCase
 {
@@ -17,5 +19,46 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_the_application_returns_404_code_on_not_found()
+    {
+        $response = $this->get('/aguacate');
+        $response->assertStatus(404);
+    }
+
+    public function test_the_new_category_route_works()
+    {
+        $response = $this->post(route('create-category', [
+            "name" => "category-test",
+        ]));
+        $this->assertDataBaseHas('categories', [
+            "name" => "category-test",
+        ]);
+        $response ->assertStatus(302);
+    }
+
+    public function test_empty_category_names_are_no_saved()
+    {
+        $category_number = Category::all()->count();
+        $response = $this->post(route('create-category', []));
+        $this->assertDatabaseCount('categories', $category_number);
+        $response->assertStatus(302);
+    }
+
+    public function test_single_product_view_show_data ()
+    {
+        $product = Product::factory()->create();
+        $related_products = Product::factory(3)->create();
+        $view = $this->view('public.single-product', [
+            "product" => $product,
+            "related_products" => $related_products,
+        ]);
+
+        $view->assertSee($product->name);
+        $view->assertSee($product->price);
+        $view->assertSee($product->content);
+        $view->assertSee($product->price);
+
     }
 }
